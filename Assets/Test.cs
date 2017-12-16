0fc5b39f3a033dfda9216fc94847dff17a64097a
sbz1 <f45rev_7pn.8c@umail.plala.or.jp>
2017-12-16 23:12:34 +0900

    ScriptTest 課題 課題：配列を宣言して出力

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        int[] array = new int[5];
+
+
+        array[4] = 715;
+        array[3] = 677;
+        array[2] = 147;
+        array[1] = 362;
+        array[0] = 521;
+       
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+
+
+
+
+}
+
+    void Update()
+    {
+
+    }
+}
